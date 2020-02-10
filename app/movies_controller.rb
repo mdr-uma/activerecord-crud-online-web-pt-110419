faf3fd016a8312c354167cfd252b5b881ec2c58d
Uma Manandhar <um_2003_8@hotmail.com>
2020-02-10 20:39:00 +0000

    Automatically backed up by Learn

--- app/movies_controller.rb
+++ app/movies_controller.rb
@@ -24,7 +24,6 @@ def can_be_created_with_a_hash_of_attributes
 end
 
 def can_be_created_in_a_block(title: "Home Alone", release_date: 1990)
-  # binding.pry
   # If no arguments are passed, use default values:
   #  title == "Home Alone"
   #  release_date == 1990
@@ -32,6 +31,7 @@ def can_be_created_in_a_block(title: "Home Alone", release_date: 1990)
        m.title = "#{title}"
        m.release_date = "#{release_date}"
     end
+    binding.pry
 end
 
 def can_get_the_first_item_in_the_database
